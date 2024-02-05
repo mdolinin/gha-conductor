0d214c4f23f297925fb8e0071659e498131b218c
mdolinin <dmo.builder@gmail.com>
2024-02-05 11:58:55 -0500

    test(app): cover PR opened no hooks triggered

--- test/index.test.ts
+++ test/index.test.ts
@@ -10,6 +10,7 @@ import payload from "./fixtures/issues.opened.json";
 import pushGhaYamlChangedPayload from "./fixtures/push.gha_yaml_changed.json";
 import deleteBranchPayload from "./fixtures/delete.branch.json";
 import pullRequestLabeledPayload from "./fixtures/pull_request.labeled.json";
+import pullRequestOpenedPayload from "./fixtures/pull_request.opened.json";
 const issueCreatedBody = { body: "Thanks for opening this issue!" };
 const fs = require("fs");
 const path = require("path");
@@ -19,6 +20,7 @@ const privateKey = fs.readFileSync(
   "utf-8"
 );
 import {GhaLoader} from "../src/gha_loader";
+import {Hooks} from "../src/hooks";
 const loadAllGhaYamlMock = jest
     .spyOn(GhaLoader.prototype, 'loadAllGhaYaml')
     .mockImplementation(() => {
@@ -28,6 +30,24 @@ const deleteAllGhaHooksForBranchMock = jest
     .spyOn(GhaLoader.prototype, 'deleteAllGhaHooksForBranch')
     .mockImplementation(() => {
       return Promise.resolve();
+    });
+
+const loadAllGhaYamlForBranchIfNewMock = jest
+    .spyOn(GhaLoader.prototype, 'loadAllGhaYamlForBranchIfNew')
+    .mockImplementation(() => {
+      return Promise.resolve();
+    });
+
+const loadGhaHooksMock = jest
+    .spyOn(GhaLoader.prototype, 'loadGhaHooks')
+    .mockImplementation(() => {
+      return Promise.resolve([])
+    });
+
+const filterTriggeredHooksMock = jest
+    .spyOn(Hooks.prototype, 'filterTriggeredHooks')
+    .mockImplementation(() => {
+      return Promise.resolve(new Set<string>());
     });
 
 describe("gha-conductor app", () => {
@@ -107,6 +127,52 @@ describe("gha-conductor app", () => {
   test("load all gha yaml files into db when PR labeled with gha-conductor:load", async () => {
     await probot.receive({ name: "pull_request", payload: pullRequestLabeledPayload });
     expect(loadAllGhaYamlMock).toHaveBeenCalledTimes(1);
+  });
+
+  test("when PR opened with files that not match any hook, create pr-status check with status completed", async () => {
+    const mock = nock("https://api.github.com")
+      // Test that we correctly return a test token
+      .post("/app/installations/44167724/access_tokens")
+      .reply(200, {
+          token: "test",
+          permissions: {
+              pull_requests: "write",
+          },
+      })
+
+      // get list of all open PRs in the repo
+      .get("/repos/mdolinin/mono-repo-example/pulls/27")
+      .reply(200, {
+          mergeable: true,
+          merge_commit_sha: "b2a4cf69f2f60bc8d91cd23dcd80bf571736dee8",
+          base: {
+              ref: "main",
+        },
+      })
+      // get pull request files
+      .get("/repos/mdolinin/mono-repo-example/pulls/27/files")
+      .reply(200, [
+          {
+              filename: "test.sh",
+          },
+      ])
+      // create check run
+      .post("/repos/mdolinin/mono-repo-example/check-runs", (body: any) => {
+          expect(body).toMatchObject({
+              name: "pr-status",
+              head_sha: "b2a4cf69f2f60bc8d91cd23dcd80bf571736dee8",
+              status: "completed",
+              conclusion: "success",
+          });
+          return true;
+      })
+      .reply(200);
+
+    await probot.receive({ name: "pull_request", payload: pullRequestOpenedPayload });
+    expect(loadAllGhaYamlForBranchIfNewMock).toHaveBeenCalledTimes(1);
+    expect(loadGhaHooksMock).toHaveBeenCalledTimes(1);
+    expect(filterTriggeredHooksMock).toHaveBeenCalledTimes(1);
+    expect(mock.pendingMocks()).toStrictEqual([]);
   });
 
   afterEach(() => {
