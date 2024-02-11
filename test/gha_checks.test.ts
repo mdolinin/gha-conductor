59a0fd4b63bf4069aba98d88ebba3eb35969dde4
mdolinin <dmo.builder@gmail.com>
2024-02-11 17:30:40 -0500

    test(app): verify create pr-status check and persist into db

--- test/gha_checks.test.ts
+++ test/gha_checks.test.ts
@@ -3,12 +3,34 @@ import pullRequestOpenedPayload from "./fixtures/pull_request.opened.json";
 import {PullRequest} from "@octokit/webhooks-types";
 
 const insertMock = jest.fn();
+const findAllMock = jest.fn().mockImplementation(() => {
+    return [
+        {
+            id: 1,
+            name: 'gha-checks-1234567890',
+            head_sha: '1234567890',
+            merge_commit_sha: '1234567890',
+            pipeline_run_name: 'gha-checks-1234567890',
+            workflow_run_inputs: {},
+            pr_number: 1,
+            hook: 'onPullRequest'
+        }
+    ]
+});
+const findMock = jest.fn().mockImplementation(() => {
+    return {
+        all: findAllMock
+    }
+});
+const updateMock = jest.fn();
 
 jest.mock('../src/db/database', () => {
     return {
         gha_workflow_runs: jest.fn(() => {
             return {
                 insert: insertMock,
+                find: findMock,
+                update: updateMock,
             };
         })
     }
@@ -75,5 +97,62 @@ describe('gha_checks', () => {
             pr_number: pullRequestOpened.number,
             hook: 'onPullRequest'
         });
+    });
+
+    it('create pr-status check if no pipelines triggered', async () => {
+        const merge_commit_sha = '1234567890';
+        let mock = jest.fn().mockImplementation(() => {
+            return {
+                data: {
+                    id: 1,
+                },
+                status: 201,
+            }
+        });
+        const octokit = {
+            checks: {
+                create: mock
+            }
+        }
+        // @ts-ignore
+        await checks.createPRCheckNoPipelinesTriggered(octokit, pullRequestOpenedPayload.pull_request, 'onBranchMerge', merge_commit_sha);
+        expect(mock).toHaveBeenCalled();
+    });
+
+    it('create pr-status check for triggered pipelines', async () => {
+        const merge_commit_sha = '1234567890';
+        let createCheckMock = jest.fn().mockImplementation(() => {
+            return {
+                data: {
+                    id: 1,
+                },
+                status: 201,
+            }
+        });
+        const downloadJobLogsForWorkflowRunMock = jest.fn().mockImplementation(() => {
+            return {
+                data: 'logs',
+                status: 200,
+            }
+        });
+        const octokit = {
+            checks: {
+                create: createCheckMock
+            },
+            actions: {
+                downloadJobLogsForWorkflowRun: downloadJobLogsForWorkflowRunMock
+            }
+        }
+        // @ts-ignore
+        await checks.createPRCheckForTriggeredPipelines(octokit, pullRequestOpenedPayload.pull_request, 'onPullRequest', merge_commit_sha);
+        expect(findMock).toHaveBeenCalledWith({
+            pr_number: pullRequestOpenedPayload.pull_request.number,
+            pr_check_id: null,
+            hook: 'onPullRequest'
+        });
+        expect(findAllMock).toHaveBeenCalled();
+        expect(createCheckMock).toHaveBeenCalledTimes(1);
+        expect(downloadJobLogsForWorkflowRunMock).toHaveBeenCalledTimes(1);
+        expect(updateMock).toHaveBeenCalledTimes(1);
     });
 });
