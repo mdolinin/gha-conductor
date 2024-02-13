af63b537b280f03a7c0253df4503b2cd6f7e95b7
mdolinin <dmo.builder@gmail.com>
2024-02-13 13:16:56 -0500

    fix(deploy): revert redundant changes get-private-key lib supports bas64 encoding by default

--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,9 @@
 import { createNodeMiddleware, createProbot } from "probot";
 import app from "./index";
 
+const probot = createProbot();
+
 export default createNodeMiddleware(app, {
-    probot: createProbot({
-        overrides: {
-            privateKey: Buffer.from(process.env.PRIVATE_KEY || "bm8gY29udGVudCAtbgo=", "base64").toString(),
-        }
-    }),
+    probot,
     webhooksPath: "/api/github/webhooks",
 });
