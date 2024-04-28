37d1b992dce445a9b09a52ea9b5cb76573ab5f8b
mdolinin <dmo.builder@gmail.com>
2024-04-28 17:36:58 -0400

    feat(slash-command): verify that run workflow on slash command will substitute command and args

--- test/hooks.test.ts
+++ test/hooks.test.ts
@@ -104,6 +104,38 @@ describe('gha hooks', () => {
         expect(findAllMock).toHaveBeenCalledTimes(1);
     });
 
+    it('find hooks to trigger, when matched files changed on slash command received', async () => {
+        let hook = {
+            repo_full_name: "repo_full_name",
+            branch: "baseBranch",
+            file_changes_matcher: "file1",
+            destination_branch_matcher: "baseBranch",
+            hook: "onSlashCommand" as HookType,
+            hook_name: "hook_name",
+            pipeline_unique_prefix: "pipeline_unique_prefix",
+            pipeline_name: "pipeline_name",
+            pipeline_ref: "pipeline_ref",
+            pipeline_params: {},
+            shared_params: {},
+            slash_command: 'validate'
+        };
+        const triggeredHookNames = await hooks.filterTriggeredHooks(
+            "repo_full_name", "onSlashCommand", ["file1", "file2"], "baseBranch",
+            [
+                hook
+            ], 'validate');
+        expect(triggeredHookNames).toEqual(new Set([hook]));
+        expect(findMock).toHaveBeenCalledWith(
+            {
+                repo_full_name: "repo_full_name",
+                branch: "baseBranch",
+                hook: "onSlashCommand",
+                slash_command: 'validate'
+            }
+        );
+        expect(findAllMock).toHaveBeenCalledTimes(1);
+    });
+
     it('should trigger correct workflow, when list of hooks provided', async () => {
         const merge_commit_sha = "0123456789abcdef";
         const workflowDispatchMock = jest.fn().mockImplementation(() => {
@@ -217,6 +249,86 @@ describe('gha hooks', () => {
                     SERIALIZED_VARIABLES: "{\"PR_HEAD_REF\":\"head_ref\",\"PR_HEAD_SHA\":\"head_sha\",\"PR_BASE_REF\":\"base_ref\",\"PR_BASE_SHA\":\"base_sha\",\"PR_MERGE_SHA\":\"0123456789abcdef\",\"PR_NUMBER\":1,\"PR_ACTION\":\"opened\",\"ROOT_DIR\":\"root_dir2\",\"shared_param\":\"shared_param\",\"COMMAND\":\"command2\",\"pipeline_param\":\"pipeline_param_2\"}",
                 },
                 name: "namespace1-module1-hook2-head_sha"
+            }
+        ]);
+    });
+
+    it('should trigger workflow with substituted command and args, for onSlashCommand when list of command tokens provided', async () => {
+        const merge_commit_sha = "0123456789abcdef";
+        const workflowDispatchMock = jest.fn().mockImplementation(() => {
+            return {
+                status: 204
+            }
+        });
+        const octokit = {
+            rest: {
+                actions: {
+                    createWorkflowDispatch: workflowDispatchMock
+                }
+            }
+        };
+        const pull_request = {
+            merged: false,
+            number: 1,
+            head: {
+                ref: "head_ref",
+                sha: "head_sha"
+            },
+            base: {
+                ref: "base_ref",
+                sha: "base_sha",
+                repo: {
+                    default_branch: "main",
+                    name: "repo_name",
+                    full_name: "repo_full_name",
+                    owner: {
+                        login: "owner_login"
+                    }
+                }
+            }
+        };
+        const triggeredHooks = new Set<GhaHook>();
+        const hook1 = {
+            branch: "hookBranch1",
+            destination_branch_matcher: "main",
+            hook_name: "hook1",
+            pipeline_name: "pipeline_name_1",
+            pipeline_params: {
+                COMMAND: 'make ${command} ${args}',
+                pipeline_param: "pipeline_param_1"
+            },
+            pipeline_ref: "feature/1",
+            repo_full_name: "repo_full_name",
+            shared_params: {
+                ROOT_DIR: "root_dir1",
+                shared_param: "shared_param"
+            },
+            pipeline_unique_prefix: "namespace1-module1-hook1",
+            file_changes_matcher: "*.yaml",
+            slash_command: 'validate',
+            hook: "onSlashCommand" as HookType
+        };
+        triggeredHooks.add(hook1);
+        // @ts-ignore
+        const triggeredPipelineNames = await hooks.runWorkflow(octokit, pull_request, "opened", triggeredHooks, merge_commit_sha, ['validate', 'arg1', 'arg2']);
+        expect(workflowDispatchMock).toHaveBeenCalledWith({
+            inputs: {
+                PIPELINE_NAME: "namespace1-module1-hook1-head_sha",
+                SERIALIZED_VARIABLES: "{\"PR_HEAD_REF\":\"head_ref\",\"PR_HEAD_SHA\":\"head_sha\",\"PR_BASE_REF\":\"base_ref\",\"PR_BASE_SHA\":\"base_sha\",\"PR_MERGE_SHA\":\"0123456789abcdef\",\"PR_NUMBER\":1,\"PR_ACTION\":\"opened\",\"ROOT_DIR\":\"root_dir1\",\"shared_param\":\"shared_param\",\"COMMAND\":\"make validate arg1 arg2\",\"pipeline_param\":\"pipeline_param_1\"}",
+            },
+            owner: "owner_login",
+            ref: "feature/1",
+            repo: "repo_name",
+            workflow_id: "pipeline_name_1.yaml"
+        });
+        expect(workflowDispatchMock).toHaveBeenCalledTimes(1);
+        expect(triggeredPipelineNames).toEqual([
+            {
+                inputs: {
+                    PIPELINE_NAME: "namespace1-module1-hook1-head_sha",
+                    SERIALIZED_VARIABLES: "{\"PR_HEAD_REF\":\"head_ref\",\"PR_HEAD_SHA\":\"head_sha\",\"PR_BASE_REF\":\"base_ref\",\"PR_BASE_SHA\":\"base_sha\",\"PR_MERGE_SHA\":\"0123456789abcdef\",\"PR_NUMBER\":1,\"PR_ACTION\":\"opened\",\"ROOT_DIR\":\"root_dir1\",\"shared_param\":\"shared_param\",\"COMMAND\":\"make validate arg1 arg2\",\"pipeline_param\":\"pipeline_param_1\"}",
+                },
+                name: "namespace1-module1-hook1-head_sha"
             }
         ]);
     });
